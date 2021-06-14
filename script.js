cc8bc7c95bb7011189c716e4aefb1da5b7928fa6
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2021-06-15 03:03:41 +0530

    change pen color and eraser implemented

--- script.js
+++ script.js
@@ -9,66 +9,113 @@ let uploadBtn = document.querySelector(".upload");
 let downloadBtn = document.querySelector(".download");
 let zoomInBtn = document.querySelector(".zoomin");
 let zoomOutBtn = document.querySelector(".zoomout");
-let isPenSelected = false;
+let penColorBtn = document.querySelector(".penColor");
 
+let isPenSelected = false;
+let isEraserSelected = false;
 let isMouseDown = false;
 let isVisible = true;
-
+let penColor = "red";
 
 board.height = window.innerHeight;
 board.width = window.innerWidth;
 tool.strokeStyle = "red";
 tool.lineWidth = 10;
 
+
+// to set the width of canvas screen
 console.log(window.innerWidth + " " + window.innerHeight);
 console.log(document.body.clientWidth + " " + document.body.clientHeight);
 
-board.addEventListener("mousedown",function(e){
-    isMouseDown = true;
-    tool.beginPath();
-    tool.moveTo(e.clientX,e.clientY);    
+// to select pen color
+penColorBtn.addEventListener("change", function (e) {
+    penColor = e.target.value;
 })
 
-board.addEventListener("mousemove",function(e){
-    if(isMouseDown){
-        tool.lineTo(e.clientX,getY(e.clientY));
-        tool.stroke();
+
+// to draw the figures
+board.addEventListener("mousedown", function (e) {
+    isMouseDown = true;
+    if (isEraserSelected) {
+        tool.globalCompositeOperation = 'destination-out';
+        tool.beginPath();
+        tool.moveTo(e.clientX, e.clientY);
+    } else {
+        tool.strokeStyle = penColor;
+        tool.beginPath();
+        tool.moveTo(e.clientX, e.clientY);
     }
 })
 
-board.addEventListener("mouseup",function(e){
-    tool.lineTo(e.clientX,e.clientY);
-    tool.stroke();
+board.addEventListener("mousemove", function (e) {
+    if (isMouseDown) {
+        if (isEraserSelected) {
+            tool.lineTo(e.clientX, getY(e.clientY));
+            tool.stroke();
+        } else {
+            tool.lineTo(e.clientX, getY(e.clientY));
+            tool.stroke();
+        }
+    }
+})
+
+board.addEventListener("mouseup", function (e) {
+    if (isEraserSelected) {
+        tool.globalCompositeOperation = 'source-over';
+    } else {
+        tool.lineTo(e.clientX, e.clientY);
+        tool.stroke();
+    }
     isMouseDown = false;
 })
 
+// for toggling the menu bar
 function myFunction(clicked) {
     clicked.classList.toggle("change");
-    if(isVisible){
+    if (isVisible) {
         topIcons.style.display = "none";
         isVisible = false;
-    }else{
+    } else {
         topIcons.style.display = "flex";
         isVisible = true;
     }
 }
 
+// getting y in case of menu bar
 function getY(y) {
-    return y-boardObj.top;
+    return y - boardObj.top;
 }
 
-penBtn.addEventListener("click",function (e) {
-    if(!isPenSelected){
+penBtn.addEventListener("click", function (e) {
+    if (!isPenSelected) {
+        removeActive();
         penBtn.classList.add("active-btn");
-        let ip = document.createElement("input");
-        ip.type = "color";
-        document.querySelector("body").append(ip);
-        ip.style.marginLeft = e.clientX+"px";
-        ip.style.marginTop = e.clientY+"px";
-        // console.log(ip.style.marginLeft + " " + ip.style.marginTop);
-        ip.click();
-    }else{
+    } else {
         penBtn.classList.remove("active-btn");
     }
     isPenSelected = !isPenSelected;
 })
+
+
+// toggle selected btw pencil and eraser
+function removeActive() {
+    if (isPenSelected) {
+        penBtn.classList.remove("active-btn");
+    } else if (isEraserSelected) {
+        eraserBtn.classList.remove("active-btn");
+    }
+}
+
+
+// for eraser
+eraserBtn.addEventListener("click", function () {
+    if (!isEraserSelected) {
+        removeActive();
+        eraserBtn.classList.add("active-btn")
+    } else {
+        eraserBtn.classList.remove("active-btn");
+    }
+    isEraserSelected = !isEraserSelected;
+})
+
+
